3271aafd99bf4461eac64e262b5abe09a6358c0a
danieleisenkoelbl <48825859+danieleisenkoelbl@users.noreply.github.com>
2019-03-22 16:02:01 +0100

    Initial commit

---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+import Layout from "../layouts/default";
+import Link from "next/link";
+
+export default () => (
+  <Layout>
+    <h1>Telefonnummer</h1>
+    <p>123456</p>
+
+    <h2>Adress</h2>
+    <p>Musterstraße 3, 1010 Wien, </p>
+    <p>Österreich</p>
+  </Layout>
+);
---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import Layout from "../layouts/default";
+
+export default () => (
+  <Layout>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+      eirmod tempor invidunt ut l
+    </p>
+    <h2>Eine Kurze Geschichte</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+      amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+      nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+      diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+      Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
+      sit amet.
+    </p>
+    <h3>Zusammenfassung</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+      clita kasd gubergren,
+    </p>
+  </Layout>
+);
